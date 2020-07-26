93f8505cea645a0c1f3bc123c78d42ecebe30094
Jakub Duchon <jakub.duchon9@gmail.com>
2020-07-26 19:58:57 +0200

    add bet form

--- pages/index.vue
+++ pages/index.vue
@@ -28,74 +28,38 @@
       </v-row>
       <v-card>
         <v-card-title class="headline">
-          The last game
+          Bet
         </v-card-title>
         <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
+          <v-form ref="form">
+            <v-radio-group v-model="resultChoiceGroup" row :rules="[]">
+              <v-row justify="space-around">
+                <v-radio :label="response.teams.home.name" value="1"></v-radio>
+                <v-radio label="Draw" value="0"></v-radio>
+                <v-radio :label="response.teams.away.name" value="2"></v-radio>
+              </v-row>
+            </v-radio-group>
+
+            <v-row>
+              <v-col cols="6">
+                <v-text-field
+                  type="number"
+                  :label="`${response.teams.home.name} score`"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field
+                  type="number"
+                  :label="`${response.teams.away.name} score`"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-form>
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn color="primary" nuxt to="/inspire">
-            Continue
+            Submit
           </v-btn>
         </v-card-actions>
       </v-card>
