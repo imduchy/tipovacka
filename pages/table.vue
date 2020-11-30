ad153cada866a54276a0ca90dc44ae106b77b978
Jakub Duchon <jakub.duchon9@gmail.com>
2020-11-30 22:39:24 +0100

    feat(table.vue): load table with group users data

---
+++ pages/table.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row>
+    <v-col cols="12">
+      <v-data-table
+        :headers="[
+          { text: 'Name', value: 'username' },
+          { text: 'Points', value: 'points' },
+          { text: 'Bets', value: 'bets' },
+        ]"
+        :items="users"
+        :sort-by="['points']"
+        :sort-desc="[true]"
+        :items-per-page="30"
+        hide-default-footer
+        class="elevation-1"
+      >
+      </v-data-table>
+    </v-col>
+  </v-row>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { IUser } from '~/models/User'
+export default Vue.extend({
+  computed: {
+    users() {
+      const users = this.$store.state.users as IUser[]
+      return users.map((u) => ({
+        username: u.username,
+        points: u.totalScore![0].score,
+        bets: u.bets?.length || 0,
+      }))
+    },
+  },
+})
+</script>
