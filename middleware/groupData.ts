b760b47500f9b9ff446721259bf0d09cbdcd36e1
Jakub Duchon <jakub.duchon9@gmail.com>
2020-12-19 02:07:51 +0100

    fix(middleware): fetch groupData on each route

--- middleware/groupData.ts
+++ middleware/groupData.ts
@@ -2,9 +2,7 @@ import { Middleware, Context } from '@nuxt/types'
 
 const groupDataMiddleware: Middleware = async ({ $auth, store }: Context) => {
   if ($auth.loggedIn) {
-    if (Object.keys(store.state.group).length === 0) {
-      await store.dispatch('fetchGroupData', $auth.user.groupId)
-    }
+    await store.dispatch('fetchGroupData', $auth.user.groupId)
   }
 }
 
