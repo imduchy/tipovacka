f724054086cb3627ebe50ab31c2ebda92ad8b43a
Jakub Duchon <jakub.duchon9@gmail.com>
2020-12-20 15:22:36 +0100

    feat(app-bar): redesign app bar

---
+++ components/Navbar.vue
@@ -0,0 +1,111 @@
+<template>
+  <span>
+    <!-- Navigation drawer -->
+    <v-navigation-drawer v-model="sidebar" temporary app>
+      <v-list>
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <img src="/user-icon.png" />
+          </v-list-item-avatar>
+
+          <v-list-item-title>{{ $auth.user.username }} </v-list-item-title>
+        </v-list-item>
+        <v-list-item v-for="item in items" :key="item.title" :to="item.to">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>{{ item.title }}</v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="logout">
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Odhlásiť</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- Navigation drawer -->
+    <v-app-bar app color="primary">
+      <span class="hidden-sm-and-up">
+        <v-app-bar-nav-icon
+          v-if="$auth.loggedIn"
+          @click="sidebar = !sidebar"
+        ></v-app-bar-nav-icon>
+      </span>
+      <v-toolbar-title
+        ><router-link to="/" tag="span" style="cursor: pointer">
+          {{ title }}
+        </router-link></v-toolbar-title
+      >
+      <v-spacer></v-spacer>
+      <template v-if="$auth.loggedIn">
+        <span class="hidden-sm-and-down">
+          <v-btn
+            v-for="item in items"
+            :key="item.title"
+            large
+            class="mr-2"
+            icon
+            :to="item.to"
+          >
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-btn>
+        </span>
+        <v-avatar color="accent" size="38" class="mr-3">
+          <img alt="Avatar" src="/user-icon.png" />
+        </v-avatar>
+        <!-- Additional menu -->
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on">
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+
+          <v-list dense>
+            <v-list-item @click="logout">
+              <v-list-item-icon>
+                <v-icon>mdi-logout</v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title>Odhlásiť</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <!-- Additional menu -->
+      </template>
+      <template v-else>
+        <span class="hidden-sm-and-down"> </span>
+      </template>
+    </v-app-bar>
+  </span>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data: () => ({
+    items: [
+      {
+        icon: 'mdi-home',
+        title: 'Domov',
+        to: '/',
+      },
+      {
+        icon: 'mdi-seal',
+        title: 'Tabuľka',
+        to: '/table',
+      },
+    ],
+    title: 'Tipovačka',
+    sidebar: false,
+  }),
+  methods: {
+    async logout() {
+      await this.$auth.logout()
+    },
+  },
+})
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,51 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="drawer" bottom fixed app>
-      <template v-if="$auth.loggedIn" v-slot:prepend>
-        <v-list-item class="px-2">
-          <v-list-item-avatar>
-            <img src="/user-icon.png" />
-          </v-list-item-avatar>
-
-          <v-list-item-title>{{ $auth.user.username }} </v-list-item-title>
-        </v-list-item>
-      </template>
-      <template v-else v-slot:prepend>
-        <v-list-item two-line>
-          <v-list-item-avatar>
-            <img src="/user-icon.png" />
-          </v-list-item-avatar>
-          <v-list-item-content>
-            <v-list-item-title>Guest</v-list-item-title>
-            <!-- <v-list-item-subtitle>Logged In</v-list-item-subtitle> -->
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-      <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-      <template v-if="$auth.loggedIn" v-slot:append>
-        <v-list-item @click="userLogout">
-          <v-list-item-action>
-            <v-icon>mdi-logout</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title>Logout</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </template>
-    </v-navigation-drawer>
-    <v-app-bar color="primary" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-toolbar-title v-text="title" />
-    </v-app-bar>
+    <Navbar></Navbar>
 
     <v-main>
       <v-container>
@@ -73,8 +28,9 @@
 <script lang="ts">
 import Vue from 'vue'
 import Alert from '../components/Alert.vue'
+import Navbar from '../components/Navbar.vue'
 export default Vue.extend({
-  components: { Alert },
+  components: { Alert, Navbar },
   data() {
     return {
       drawer: false,
