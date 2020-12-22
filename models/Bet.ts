3fc63dbad07255e35ea06036b1287ee507162c61
Jakub Duchon <jakub.duchon9@gmail.com>
2020-12-22 20:56:34 +0000

    feat(Bet): add BetStatus to Bet model

--- models/Bet.ts
+++ models/Bet.ts
@@ -2,11 +2,17 @@ import mongoose, { Schema } from 'mongoose'
 import { IGame } from './Game'
 import { IUser } from './User'
 
+enum BetStatus {
+  'EVALUATED',
+  'PENDING',
+}
+
 export interface IBet {
   homeTeamScore: number
   awayTeamScore: number
   game: IGame | mongoose.Types.ObjectId
   user: IUser | mongoose.Types.ObjectId
+  status?: BetStatus
 }
 
 // export type IBetDocument = IBet & Document
@@ -18,6 +24,7 @@ export const BetSchema = new Schema(
     awayTeamScore: { type: Number, required: true, default: 1 },
     game: { type: Schema.Types.ObjectId, ref: 'game' },
     user: { type: Schema.Types.ObjectId, ref: 'user' },
+    status: { type: BetStatus, default: BetStatus.PENDING },
   },
   {
     timestamps: true,
