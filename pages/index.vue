6c1057e653068202cf845e2285e87c411bd818fd
Jakub Duchon <jakub.duchon9@gmail.com>
2020-12-25 14:43:05 +0000

    feat(index): add tooltip for last bets

--- pages/index.vue
+++ pages/index.vue
@@ -21,7 +21,7 @@
           <!-- Upcoming game -->
           <upcoming-game :upcoming-game="upcomingGame"></upcoming-game>
 
-          <v-row>
+          <v-row class="mb-5">
             <v-col cols="12" class="pt-0">
               <v-card class="pt-8 px-8">
                 <!-- Input field / Current bet -->
@@ -31,7 +31,19 @@
             </v-col>
           </v-row>
           <!-- Last bets -->
-          <div class="mt-5 text-h5">Posledné tipy</div>
+          <div class="mt-5 text-h5">
+            Posledné tipy
+
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon color="grey darken-2" medium v-bind="attrs" v-on="on">
+                  mdi-information-outline
+                </v-icon>
+              </template>
+              <span>Tvoj tip je v zátvorkách vedľa výsledku zápasu</span>
+            </v-tooltip>
+          </div>
+
           <v-row>
             <v-col v-if="evaluatedBets[2]" cols="12" lg="4">
               <user-bet :bet="usersBets[2]"></user-bet>
