f19a4499ad7d6a9867e1d449ce6cb7a0e1ad7060
Jakub Duchon <jakub.duchon9@gmail.com>
2021-01-24 17:47:25 +0100

    feat(logs): output logs into console instead of file

--- utils/logger.ts
+++ utils/logger.ts
@@ -4,26 +4,15 @@ const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   defaultMeta: { service: 'user-service' },
-  transports: [],
-})
-
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-if (process.env.NODE_ENV !== 'production') {
-  logger.add(
+  transports: [
     new winston.transports.Console({
       format: winston.format.combine(
         format.label(),
         format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
       ),
-    })
-  )
-} else {
-  // Write all logs with level `error` and below to `error.log`, and
-  // Write all logs with level `info` and below to `combined.log`
-  logger.add(new winston.transports.File({ filename: 'logs/error.log', level: 'error' }))
-  logger.add(new winston.transports.File({ filename: 'logs/combined.log' }))
-}
+    }),
+  ],
+})
 
 export default logger
