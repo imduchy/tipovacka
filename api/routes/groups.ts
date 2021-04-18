88b9a33a2bdac34c296ddf149176ed67935b3b4e
Jakub Duchon <jakub.duchon9@gmail.com>
2021-04-18 22:01:41 +0200

    feat(api): add /groups/competition endpoint

--- api/routes/groups.ts
+++ api/routes/groups.ts
@@ -7,6 +7,8 @@ import logger from '../utils/logger';
 const router = Router();
 
 const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  logger.info(`[${req.method}] ${req.baseUrl}${req.path} from ${req.ip}.`);
+
   // If req.headers contains the admin key, continue
   if (isAdmin(req)) {
     next();
@@ -15,7 +17,7 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 
   if (isLoggedIn(req)) {
     const user = req.user as IUser;
-    // Only allow user to access his own User object
+    // Only allow users to access Group document of a group they're part of
     if (user.groupId!.equals(req.params.groupId)) {
       next();
       return;
@@ -31,8 +33,14 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 };
 
 /**
- * Get users of a group by group's _id
- * Access: External function
+ * Get a group's users.
+ *
+ * Access: Protected (Logged-in & Part of the group)
+ *
+ * @param username username of the newly created user
+ * @param email email of the newly created user
+ * @param password password of the newly created user
+ * @param group ObjectId of the group the user will be part of
  */
 router.get('/:groupId/users', authMiddleware, async (req, res) => {
   try {
@@ -62,7 +70,62 @@ router.get('/:groupId', authMiddleware, async (req, res) => {
     }
     res.status(200).json(group);
   } catch (error) {
-    logger.error(`Couldn't fetch a group ${req.params.groupId}. Error: ${error}`);
+    logger.error(`Couldn't fetch a group ${groupId}. Error: ${error}`);
+    res.status(500).json('Internal server error');
+  }
+});
+
+/**
+ * Get a competition.
+ *
+ * Access: Protected (Logged-in & Part of the group)
+ *
+ * @param group ObjectId of the group to be fetched
+ * @param team team API Id to filter on
+ * @param season season to filter on
+ * @param competition competition API Id to filter on
+ */
+router.get('/competition', authMiddleware, async (req, res) => {
+  const q = req.query;
+
+  if (!q.group || !q.team || !q.competition || !q.season) {
+    logger.info('Not all required query parameters were specified. ' + JSON.stringify(q));
+    return res
+      .status(403)
+      .json('Query parameters group, team, competition and season are required!');
+  }
+
+  try {
+    const groupId = q.group.toString();
+    const teamApiId = parseInt(q.team.toString());
+    const competitionApiId = parseInt(q.competition.toString());
+    const season = parseInt(q.season.toString());
+
+    if (!Types.ObjectId.isValid(groupId)) {
+      logger.info('Provided group id is of a wrong format. Provided value: ' + q.group);
+      return res.status(403).json('Invalid value for the group parameter.');
+    }
+
+    const aggrResult = await Group.aggregate()
+      .match({ _id: Types.ObjectId(groupId) })
+      .unwind('followedTeams')
+      .unwind('followedTeams.seasons')
+      .unwind('followedTeams.seasons.competitions')
+      .match({ 'followedTeams.apiId': teamApiId })
+      .match({ 'followedTeams.seasons.season': season })
+      .match({ 'followedTeams.seasons.competitions.apiId': competitionApiId })
+      .replaceRoot('$followedTeams.seasons.competitions');
+
+    if (!aggrResult[0]) {
+      logger.info(
+        'No competition found for the specified parameters. ' + JSON.stringify(q)
+      );
+      return res.status(404).json('Competition not found.');
+    }
+
+    res.status(200).json(aggrResult[0]);
+  } catch (error) {
+    logger.error(`Couldn't fetch group ${req.params.groupId}. Error: ${error}.`);
     res.status(500).json('Internal server error');
   }
 });
