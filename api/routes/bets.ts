88fce6877df5f22c3618666bdd485af908023a1c
Jakub Duchon <jakub.duchon9@gmail.com>
2021-06-05 21:24:09 +0200

    refactor(api): update /bets endpoints

--- api/routes/bets.ts
+++ api/routes/bets.ts
@@ -18,7 +18,7 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
   if (isLoggedIn(req)) {
     const user = req.user as IUser & { _id: Types.ObjectId };
     // Only allow user to create bet if user ids match
-    if (user._id!.equals(req.body.userId)) {
+    if (user._id.equals(req.body.user)) {
       return next();
     }
   }
@@ -41,44 +41,46 @@ const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
 router.post('/', authMiddleware, async ({ body }, res) => {
   logger.info('Data received in the request body: ' + JSON.stringify(body));
 
-  const gameId = body.game;
-  const userId = body.user;
+  const { user: userId, game: gameId, homeTeamScore, awayTeamScore, scorer } = body;
 
-  if (!gameId || !userId || !body.homeTeamScore || !body.awayTeamScore || !body.scorer) {
+  console.log('REQ', gameId, userId, homeTeamScore, awayTeamScore, scorer);
+  if (
+    gameId === undefined ||
+    userId === undefined ||
+    homeTeamScore === undefined ||
+    awayTeamScore === undefined ||
+    scorer === undefined
+  ) {
     logger.warn(
       "The request doesn't contain required request body. The bet won't be submitted."
     );
-    res.status(400).send('Bad request.');
+    return res.status(400).send('Bad request');
   }
 
   try {
     const user = await User.findById(userId);
     if (!user) {
       logger.warn("The specified user doesn't exist. The bet won't be submitted.");
-      res.status(404).json("The specified resource doesn't exist.");
-      return;
+      return res.status(404).json("The specified resource doesn't exist");
     }
 
     if (alreadyBet(user, gameId)) {
       logger.warn(
         "The specified user has already placed a bet on this game. The bet won't be submitted."
       );
-      res.status(400).json('Bad request.');
-      return;
+      return res.status(400).json('Bad request');
     }
 
     const game = await Game.findById(gameId);
     if (!game) {
       logger.warn("The specified game doesn't exist. The bet won't be submitted.");
-      res.status(404).json("The specified resource doesn't exist.");
-      return;
+      return res.status(404).json("The specified resource doesn't exist");
     }
 
     // Don't submit the bet if the game already started.
     if (new Date().getTime() > game.date.getTime()) {
       logger.warn("The specified game has already started. The bet won't be submitted.");
-      res.status(400).json('Bad request.');
-      return;
+      return res.status(400).json('Bad request');
     }
 
     const bet: IBet = {
