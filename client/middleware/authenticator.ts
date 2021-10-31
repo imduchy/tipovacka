c77b804ed6df449b8ec24c791bc5b6d2478b2701
Jakub Duchon <jakub.duchon9@gmail.com>
2021-10-31 19:16:47 +0100

    chore: bump tipovacka-models to the latest version

--- client/middleware/authenticator.ts
+++ client/middleware/authenticator.ts
@@ -2,7 +2,7 @@ import { IUser } from '@duchynko/tipovacka-models';
 import { Middleware, Context } from '@nuxt/types';
 
 const authenticatorMiddleware: Middleware = ({ $auth, error }: Context) => {
-  const user = $auth.user as IUser & { scope: string };
+  const user = $auth.user as IUser;
 
   if (!user || user.scope !== 'admin') {
     return error({ statusCode: 404, message: 'Stránka neexistuje' });
