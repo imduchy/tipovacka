a726b0e28f79a1389d235375aa444c57aff01d0c
Jakub Duchon <jakub.duchon9@gmail.com>
2022-02-13 16:42:00 +0100

    fix(engine): add libertyTarget property to webpack's output config

--- apps/engine/webpack.config.js
+++ apps/engine/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "[name]/index.js"
+    filename: "[name]/index.js",
+    libraryTarget: 'commonjs'
   },
 };
