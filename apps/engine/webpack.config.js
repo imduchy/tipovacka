2264f597a4551bf6b5c91f45f6f4bd234c443bd6
Jakub Duchon <jakub.duchon9@gmail.com>
2022-02-13 16:18:44 +0100

    fix(engine): add webpack config

---
+++ apps/engine/webpack.config.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+var path = require("path");
+
+module.exports = {
+  entry: {
+    UpdateGame: './UpdateGame/index.ts',
+    UpdateCompetition: './UpdateCompetition/index.ts',
+    GetUpcomingGame: './GetUpcomingGame/index.ts',
+    EvaluateBets: './EvaluateBets/index.ts'
+  },
+  mode: "production",
+  target: 'node',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name]/index.js"
+  },
+};
