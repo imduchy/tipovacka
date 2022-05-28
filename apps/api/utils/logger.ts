7302321d40fbaf4df806b4c8615f6e331b52f3e0
Jakub Duchon <jakub.duchon9@gmail.com>
2022-05-28 16:04:17 +0200

    chore(api): update logger settings

--- apps/api/utils/logger.ts
+++ apps/api/utils/logger.ts
@@ -1,17 +1,31 @@
 import winston, { format } from 'winston';
 
-export default winston.createLogger({
+const logger = winston.createLogger({
   level: 'info',
-  format: winston.format.json(),
-  defaultMeta: { service: 'user-service' },
-  transports: [
+  transports: [],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
     new winston.transports.Console({
       format: winston.format.combine(
-        format.splat(),
-        format.label(),
+        format.colorize(),
+        format.prettyPrint({ depth: 10 }),
         format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-        format.printf((info) => `${info.timestamp} ${info.level}: ${info.message}`)
+        format.json(),
+        format.printf((info) => `[${info.level}]: ${info.message}`)
       ),
-    }),
-  ],
-});
+    })
+  );
+} else {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.combine(
+        format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        format.printf((info) => `[${info.level}] ${info.timestamp}: ${info.message}`)
+      ),
+    })
+  );
+}
+
+export default logger;
