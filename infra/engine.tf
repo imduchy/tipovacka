92bf0597527376968a3af4961db07190c0b64c73
Jakub Duchon <jakub.duchon9@gmail.com>
2022-12-27 13:33:12 +0100

    chore(infra): add tags to engine resources

--- infra/engine.tf
+++ infra/engine.tf
@@ -4,6 +4,7 @@ resource "azurerm_storage_account" "engine" {
   location                 = azurerm_resource_group.this.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  tags                     = local.default_tags
 }
 
 resource "azurerm_service_plan" "this" {
@@ -12,12 +13,14 @@ resource "azurerm_service_plan" "this" {
   location            = azurerm_resource_group.this.location
   os_type             = "Linux"
   sku_name            = "Y1"
+  tags                = local.default_tags
 }
 
 resource "azurerm_linux_function_app" "example" {
   name                = "func-${var.project_name}-engine-${var.environment}"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
+  tags                = local.default_tags
 
   storage_account_name       = azurerm_storage_account.engine.name
   storage_account_access_key = azurerm_storage_account.engine.primary_access_key
@@ -41,6 +44,6 @@ resource "azurerm_linux_function_app" "example" {
   }
 
   app_settings = {
-    API_FOOTBALL_HOST    = "v3.football.api-sports.io"
+    API_FOOTBALL_HOST = "v3.football.api-sports.io"
   }
 }
