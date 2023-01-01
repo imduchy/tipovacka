61e3d8339fb4a2c4e0077fab6b482339a48287b3
Jakub Duchon <jakub.duchon9@gmail.com>
2023-01-02 00:22:14 +0100

    feat(engine): add trycatch block to timer trigger func

--- apps/engineV2/timerStarter/index.ts
+++ apps/engineV2/timerStarter/index.ts
@@ -2,10 +2,15 @@
 import { AzureFunction, Context } from '@azure/functions';
 
 const timerStart: AzureFunction = async function (context: Context): Promise<any> {
-  const client = df.getClient(context);
-  const instanceId = await client.startNew('orchestrator');
+  try {
+    context.log(`Timer trigger executed at ${new Date().toUTCString()}`);
+    const client = df.getClient(context);
+    const instanceId = await client.startNew('orchestrator');
 
-  context.log(`Started orchestration with ID = '${instanceId}'.`);
+    context.log(`Started orchestration with ID = '${instanceId}'.`);
+  } catch (error) {
+    context.log.error('An error occurred during excution of timer trigger. Error: ' + error);
+  }
 };
 
 export default timerStart;
